7217cb06e6668d72760ba335163ad408074f90ad
John Doe <YashchukAlex5@gmail.com>
2018-11-25 18:58:51 +0200

    test

--- src/modules/module.js
+++ src/modules/module.js
@@ -1,19 +1,36 @@
-const getNameWithPrefix = function (name = 'Masha', pref) {
-  return name + ' ' + pref;
-};
+// const getNameWithPrefix = function (name = 'Masha', pref) {
+//   return name + ' ' + pref;
+// };
+//
+// const someVariable = 30;
+//
+// const baseImport = {
+//   name: 'Base export default',
+//   gerName: function () {
+//     return this.name;
+//   },
+//   getNameWithPrefix: getNameWithPrefix
+// }
+//
+// export { getNameWithPrefix, someVariable as variable };
+//
+// export const name = 'Name';
+//
+// export default baseImport;
 
-const someVariable = 30;
-
-const baseImport = {
-  name: 'Base export default',
-  gerName: function () {
-    return this.name;
-  },
-  getNameWithPrefix: getNameWithPrefix
+function GetNotebook(callBack) {
+  setTimeout(()=> {callBack('notebook'),1000})
 }
-
-export { getNameWithPrefix, someVariable as variable };
-
-export const name = 'Name';
-
-export default baseImport;
+function GetAsus(callBack,notebook) {
+  setTimeout(()=> {callback([...notebook,'asus']),1000})
+}
+// function GetAsus501a() {
+//     return GetNotebook(function (notebook) {
+//         return GetAsus(notebook, function (asus) {
+//             return setTimeout(() => {
+//                 const result =
+//             })
+//             }
+//         )
+//     })
+// }
