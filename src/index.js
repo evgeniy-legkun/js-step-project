bc1a9cc34ea22c7c87ec606136c4cbb45021b8d2
Evgeniy Legkun <evgeniy.legkun@gmail.com>
2018-12-09 16:19:59 +0200

    updated project configuration using create-reat-app

--- src/index.js
+++ src/index.js
@@ -1,53 +1,12 @@
-// ASYNC JS
-import axios from 'axios';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// PROMISES
-new Promise((resolve, reject) => {
-  axios({
-    method: 'get',
-    url: 'https://jsonplaceholder.typicode.com/users'
-  }).then((result) => {
-    resolve(result);
-  }).catch((err) => {
-    reject(err);
-  });
-})
-  .then((result) => {
-    const users = result.data;
-    if (Array.isArray(users) && users.length !== 0) {
-      return users.filter((user) => { return user.website === 'elvis.io' })[0] || [];
-    } else {
-      return [];
-    }
-  })
-  .then((user) => {
-    return axios({
-      method: 'get',
-      url: 'https://jsonplaceholder.typicode.com/users/' + user.id
-    }).then((result) => {
-      return result.data;
-    }).catch((err) => {
-      return err;
-    });
-  })
-  .then((dataOfNeededUser) => {
-    // console.log('USER DATA (Promise)', dataOfNeededUser);
-  })
-  .catch((error) => {
-    // console.log('Error (Promise)', error);
-  });
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// Async / Await
-async function getUsers() {
-  const result = await axios.get('https://jsonplaceholder.typicode.com/users');
-  const users = result.data;
-  console.log('USERS', users);
-}
-
-async function getData () {
-  getUsers().then(() => {
-    console.log('Code after getUsers function !!!!!!!!!!!!!!!');
-  });
-}
-
-getData();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
