5eef7bc34a5c2c81ae76f44a0c97fd30a0434661
oleg <fod@ua.fm>
2018-12-09 20:56:39 +0200

    end 09/12/2018

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDom.render(<App/>, document.getElementById('root'));
+
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -10,3 +19,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
