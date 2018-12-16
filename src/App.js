a964c8ad8ac2d94e5a94fecfcf746d7f611b4ac0
Evgeniy Legkun <evgeniy.legkun@gmail.com>
2018-12-16 20:36:56 +0200

    added removing, spinner

--- src/App.js
+++ src/App.js
@@ -2,23 +2,54 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import TableUsersJobs from './components/Display/Table/TableWrapper';
+import { RingLoader } from 'react-spinners';
 
 class App extends Component {
   state = {
-
-  };
-
-  render () {
-    const usersJobsData = [
+    usersJobsData: [
       {name: 'Charlie', job: 'Janitor'},
       {name: 'Mac', job: 'Bouncer'},
       {name: 'Dee', job: 'Aspiring actress'},
       {name: 'Dennis', job: 'Bartender'}
-    ];
+    ],
+    loading: false
+  };
 
+  removeUser = (index, delay = 500) => {
+    const {usersJobsData} = this.state;
+
+    this.setState({loading: true});
+
+    setTimeout(() => {
+      this.setState({
+        usersJobsData: usersJobsData.filter((user, userIndex) => {
+          return userIndex !== index;
+        }),
+        loading: false
+      });
+    }, delay);
+  };
+
+  render () {
     return (
       <div className="container">
-        <TableUsersJobs usersJobs={usersJobsData} />
+        {
+          this.state.loading ? (
+            <div className={'spinner-container'}>
+              <RingLoader
+                loading={this.state.loading}
+                sizeUnit={'px'}
+                size={25}
+                color={'rgb(54, 215, 183)'}
+              />
+            </div>
+          ) : (
+            <TableUsersJobs
+              usersJobs={this.state.usersJobsData}
+              removeUser={this.removeUser}
+            />
+          )
+        }
       </div>
     );
   }
--- src/components/Display/Table/TableWrapper.js
+++ src/components/Display/Table/TableWrapper.js
@@ -1,32 +1,54 @@
 import React, { Component } from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Button } from 'react-bootstrap';
 import './Table.css';
 
 class TableWrapper extends Component {
   render () {
-    const { usersJobs } = this.props;
-    const rows = usersJobs.map((row, index) => {
-      return (
-        <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
-      );
-    });
+    const { usersJobs, removeUser } = this.props;
+    const createTableBody = (usersJobs) => {
+      return usersJobs.map((user, index) => {
+        return (
+          <tr key={index}>
+            <td>{user.name}</td>
+            <td>{user.job}</td>
+            <td>
+              <Button
+                bsStyle='primary'
+                bsSize='xsmall'
+                onClick={() => removeUser(index, 1000)}
+              >
+                remove
+              </Button>
+            </td>
+          </tr>
+        );
+      });
+    };
 
     return (
-      <div className='tableContainer'>
-        <Table striped bordered hover>
-          <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-          </thead>
-          <tbody>
-            {rows}
-          </tbody>
-        </Table>
+      <div>
+        {
+          usersJobs.length !== 0 ? (
+            <div className='table-container'>
+              <Table striped bordered hover className='styled-table'>
+                <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Job</th>
+                  <th>Remove</th>
+                </tr>
+                </thead>
+                <tbody>
+                  {createTableBody(usersJobs)}
+                </tbody>
+              </Table>
+            </div>
+          ) : (
+            <div className='table-empty'>
+              The table is empty
+            </div>
+          )
+        }
       </div>
     );
   }
