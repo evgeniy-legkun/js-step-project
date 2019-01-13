537f835d3531ff173b75b20e4bfd8c174410e7b5
Evgeniy Legkun <evgeniy.legkun@gmail.com>
2019-01-13 19:13:20 +0200

    added form for user creating

---
+++ src/views/Users/UserCreateForm.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+class UserCreateForm extends Component {
+  render () {
+    const {jobs} = this.props;
+
+    const formBlock = {
+      width: '350px'
+    };
+
+    const createJobsList = (jobs) => {
+      return jobs.map((job, index) => {
+        return (
+          <option key={index} value={job.name}>{job.name}</option>
+        );
+      });
+    };
+
+    return (
+      <div style={formBlock}>
+        <label htmlFor="user_name">Name : </label>
+        <input type="text" name="user_name" id="user_name" />
+        <br/><br/>
+
+        <label htmlFor="user_job">Job : </label>
+        <select name="user_job" id="user_job">
+          { createJobsList(jobs) }
+        </select>
+      </div>
+    );
+  }
+}
+
+export default UserCreateForm;
--- src/views/Users/UsersList.js
+++ src/views/Users/UsersList.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
 import { Table, Button } from 'react-bootstrap';
 import './UsersList.css';
+import UserCreateForm from './UserCreateForm';
 
 class UsersList extends Component {
   constructor (props) {
     super(props);
-    this.removeUser = this.removeUser.bind(this);
   }
 
   state = {
     users: [
-      {name: 'Charlie', job: 'Janitor'},
-      {name: 'Mac', job: 'Bouncer'},
-      {name: 'Dee', job: 'Aspiring actress'},
-      {name: 'Dennis', job: 'Bartender'}
+      {id: 1, name: 'Charlie', jobId: 1},
+      {id: 2, name: 'Mac', jobId: 2},
+      {id: 3, name: 'Dee', jobId: 3},
+      {id: 4, name: 'Dennis', jobId: 4}
     ],
-    loading: false
-  };
 
-  removeUser = (index, delay = 500) => {
+    jobs: [
+      {id: 1, name: 'Janitor'},
+      {id: 2, name: 'Bouncer'},
+      {id: 3, name: 'Aspiring actress'},
+      {id: 4, name: 'Bartender'},
+    ],
+
+    isVisibleList: true
+  }
+
+  getJobById (id) {
+    const {jobs} = this.state;
+
+    return jobs.find((job) => {
+      if (job.id === id) { return job; }
+    }) || {};
+  }
+
+  removeUser (index, delay = 500) {
     const { users } = this.state;
-    this.props.setLoader(true);
+    const { setLoader } = this.props;
+    setLoader(true);
 
     setTimeout(() => {
       this.setState({
@@ -29,19 +46,24 @@ class UsersList extends Component {
         })
       });
 
-      this.props.setLoader(false);
+      setLoader(false);
     }, delay);
-  };
+  }
+
+  handleCreateForm (value) {
+    if (typeof(value) !== 'boolean') { return; }
+    this.setState({isVisibleList: value});
+  }
 
   render () {
-    const { users } = this.state;
+    const { users, jobs, isVisibleList } = this.state;
 
     const createTableBody = (users) => {
       return users.map((user, index) => {
         return (
           <tr key={index}>
             <td>{user.name}</td>
-            <td>{user.job}</td>
+            <td>{this.getJobById(user.jobId).name}</td>
             <td>
               <Button
                 bsStyle='primary'
@@ -61,18 +83,33 @@ class UsersList extends Component {
         {
           users.length !== 0 ? (
             <div className='table-container'>
-              <Table striped bordered hover className='styled-table'>
-                <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Job</th>
-                  <th>Remove</th>
-                </tr>
-                </thead>
-                <tbody>
-                  { createTableBody(users) }
-                </tbody>
-              </Table>
+              <Button
+                style={{marginBottom: '20px'}}
+                bsStyle='primary'
+                bsSize='xsmall'
+                onClick={() => this.handleCreateForm(!isVisibleList)}
+              >
+                Add user
+              </Button>
+
+              {
+                isVisibleList ? (
+                  <Table striped bordered hover className='styled-table'>
+                    <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Job</th>
+                      <th>Remove</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    { createTableBody(users) }
+                    </tbody>
+                  </Table>
+                ) : (
+                  <UserCreateForm jobs={jobs} />
+                )
+              }
             </div>
           ) : (
             <div className='table-empty'>
